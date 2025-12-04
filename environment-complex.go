f86a38d0ff20d3486c558911caf4c87a2b05aed6
pt751898577912timothywatkins <nil_k80841446@rambler.ru>
2025-12-04 18:11:57 +0800

    add complex.adoc, environment-complex.go, @base-org+account+1.1.1.patch

---
+++ environment-complex.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\environment-complex.go
+// Generated at: 2025-10-13T02:04:38.022Z
---
+++ inventory-serializer.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\inventory-serializer.rs
+// Generated at: 2025-10-13T02:04:38.030Z
